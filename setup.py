d3ec7d267e9e62685df0e0ac9e8a8d2cc521c837
oscar <oscargwilkins@gmail.com>
2020-10-30 16:56:42 +0000

    fix file structure maybe

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ultraplex",
-    version="1.0.2",
+    version="1.0.3",
     description="fastq demultiplexer",
     long_description=README,
     long_description_content_type="text/markdown",
