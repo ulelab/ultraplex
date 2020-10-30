4bd182956ec47fe3ac6bd259a6be6a9d3447dd20
Oscar <oscargwilkins@gmail.com>
2020-10-30 16:44:46 +0000

    parent

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(__file__)
+HERE = pathlib.Path(__file__).parent
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
