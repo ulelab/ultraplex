5ae833e626a8160d1fdb69faeed5543c0a4a60b5
oscar <oscargwilkins@gmail.com>
2020-10-30 14:54:42 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="ultraplex",
+    version="0.8.0",
+    description="fastq demultiplexer",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ulelab/ultraplex.git",
+    author="Oscar Wilkins",
+    author_email="oscar.wilkins@crick.ac.uk",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["ultraplex"],
+    include_package_data=True,
+    install_requires=["cutadapt", "xopen", "dnaio"],
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
