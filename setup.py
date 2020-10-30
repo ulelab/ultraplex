719c3ade142aace62f3ac76c39b696ca9c3b914c
Oscar <oscargwilkins@gmail.com>
2020-10-30 16:40:18 +0000

    1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import pathlib
 from setuptools import setup
 
 # The directory containing this file
-HERE = pathlib.Path(__file__).parent
+HERE = pathlib.Path(__file__)
 
 # The text of the README file
 README = (HERE / "README.md").read_text()
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ultraplex",
-    version="1.0.0",
+    version="1.0.1",
     description="fastq demultiplexer",
     long_description=README,
     long_description_content_type="text/markdown",
