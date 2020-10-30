cf5723c0fc43325166d28e7a635d4f41f5200599
Delayed-Gitification <64151406+Delayed-Gitification@users.noreply.github.com>
2020-10-30 16:21:32 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ultraplex",
-    version="0.8.4",
+    version="1.0.0",
     description="fastq demultiplexer",
     long_description=README,
     long_description_content_type="text/markdown",
