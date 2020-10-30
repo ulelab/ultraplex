e0b136cd315e5ff8e475665449f1747483bc6c5e
Oscar <oscargwilkins@gmail.com>
2020-10-30 16:01:05 +0000

    try again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="ultraplex",
-    version="0.8.2",
+    version="0.8.3",
     description="fastq demultiplexer",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -28,7 +28,7 @@ setup(
     install_requires=["cutadapt==2.10"],
     entry_points={
         "console_scripts": [
-            "ultraplex = __main__.py",
+            "ultraplex = package.module:__main__",
         ]
     },
 )
